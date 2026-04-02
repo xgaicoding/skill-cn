09225aef296937c8e478c96f2800d8aa04be00b7
Li Xiao <3991571461@qq.com>
2026-04-02 15:12:02 +0800

    feat: add /egg easter egg page

---
+++ app/egg/page.tsx
@@ -0,0 +1,16 @@
+export default function EggPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "60vh",
+        fontSize: "1.5rem",
+        color: "#666",
+      }}
+    >
+      🥚 这是个彩蛋
+    </div>
+  );
+}
